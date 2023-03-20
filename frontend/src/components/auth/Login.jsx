c36554bee5baf7605ad8481b0bf98480f76451d3
Levis <anton_levus@mail.ru>
2023-03-20 11:50:14 +0530

    revert login changes

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import {
-  Row, Button, Form, InputGroup, Col, Container,
+  Row, Button, Form, InputGroup,
 } from 'react-bootstrap';
 import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
@@ -56,76 +56,69 @@ const Login = () => {
   });
 
   return (
-    <Container>
-      <Row>
-        <Col className="auth_header">
-          {t('login.pages_data.title')}
-        </Col>
+    <div className="login_container">
+      <Row className="auth_header">
+        {t('login.pages_data.title')}
       </Row>
       <Row className="form_wrapper">
-        <Col>
-          <Form onSubmit={formik.handleSubmit} className="form_container">
 
-            <Form.Group controlId="name">
-              <InputGroup hasValidation size="md">
-                <Form.Label
-                  visuallyHidden
-                >
-                  {t('login.forms.name')}
-                </Form.Label>
-                <Form.Control
-                  name="name"
-                  id="form"
-                  type="text"
-                  required
-                  placeholder={t('login.forms.name')}
-                  isInvalid={!!authError}
-                  onChange={formik.handleChange}
-                  value={formik.values.name}
-                />
-              </InputGroup>
-            </Form.Group>
+        <Form onSubmit={formik.handleSubmit} className="form_container">
 
-            <Form.Group controlId="password">
-              <InputGroup hasValidation size="md">
-                <Form.Label
-                  visuallyHidden
-                >
-                  {t('login.forms.password')}
-                </Form.Label>
-                <Form.Control
-                  name="password"
-                  id="form"
-                  type="password"
-                  required
-                  placeholder={t('login.forms.password')}
-                  isInvalid={!!authError}
-                  onChange={formik.handleChange}
-                  value={formik.values.password}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {authError}
-                </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
+          <Form.Group controlId="name">
+            <InputGroup hasValidation>
+              <Form.Label
+                visuallyHidden
+              >
+                {t('login.forms.name')}
+              </Form.Label>
+              <Form.Control
+                name="name"
+                type="text"
+                required
+                placeholder={t('login.forms.name')}
+                isInvalid={!!authError}
+                onChange={formik.handleChange}
+                value={formik.values.name}
+              />
+            </InputGroup>
+          </Form.Group>
 
-            <Button
-              type="submit"
-              variant="primary"
-              className="auth_button"
-            >
-              {t('login.pages_data.button')}
-            </Button>
-          </Form>
-        </Col>
+          <Form.Group controlId="password">
+            <InputGroup hasValidation>
+              <Form.Label
+                visuallyHidden
+              >
+                {t('login.forms.password')}
+              </Form.Label>
+              <Form.Control
+                name="password"
+                type="text"
+                required
+                placeholder={t('login.forms.password')}
+                isInvalid={!!authError}
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
+              <Form.Control.Feedback type="invalid">
+                {authError}
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+
+          <Button
+            type="submit"
+            variant="primary"
+            className="primary_button"
+          >
+            {t('login.pages_data.button')}
+          </Button>
+        </Form>
       </Row>
-      <Row>
-        <Col className="auth_footer">
-          {t('login.pages_data.footer_description')}
-          <a href="/signup" className="link">{t('login.pages_data.footer_link')}</a>
-        </Col>
+      <Row className="auth_footer">
+        {t('login.pages_data.footer_description')}
+        <a href="/signup" className="link">{t('login.pages_data.footer_link')}</a>
       </Row>
-    </Container>
+    </div>
   );
 };
 
