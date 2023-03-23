1d7a5e0fcd9c2b01a2573522f7af4d8d35c134c2
Levis <anton_levus@mail.ru>
2023-03-23 16:43:28 +0530

    edited login component styles

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -4,10 +4,9 @@ import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import {
-  Row, Button, Form, InputGroup,
+  Row, Col, Button, Form, InputGroup, FloatingLabel, Container,
 } from 'react-bootstrap';
 import Header from '../header/Header';
-import LayoutContainer from '../layoutContainer/LayoutContainer';
 import AuthService from '../../api/auth';
 import { setNotificationProps } from '../../store/slices/notification/notificationSlice';
 import './style.css';
@@ -53,71 +52,74 @@ const Login = () => {
   return (
     <>
       <Header />
-      <LayoutContainer auth height="50%">
-        <div className="login_container">
-          <Row className="auth_header">
+      <Container>
+        <Row>
+          <Col lg={6} md={8} sm={10} xs={10} className="auth_header">
             {t('login.pages_data.title')}
-          </Row>
-          <Row className="form_wrapper">
+          </Col>
+        </Row>
 
-            <Form onSubmit={formik.handleSubmit} className="form_container">
-
+        <Row>
+          <Col lg={6} md={8} sm={10} xs={10}>
+            <Form onSubmit={formik.handleSubmit}>
               <Form.Group controlId="name">
                 <InputGroup hasValidation>
-                  <Form.Label
-                    visuallyHidden
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label={t('login.forms.name')}
+                    className="mb-3"
                   >
-                    {t('login.forms.name')}
-                  </Form.Label>
-                  <Form.Control
-                    name="name"
-                    type="text"
-                    required
-                    placeholder={t('login.forms.name')}
-                    isInvalid={!!authError}
-                    onChange={formik.handleChange}
-                    value={formik.values.name}
-                  />
+                    <Form.Control
+                      name="name"
+                      type="text"
+                      required
+                      placeholder={t('login.forms.name')}
+                      isInvalid={!!authError}
+                      onChange={formik.handleChange}
+                      value={formik.values.name}
+                    />
+                  </FloatingLabel>
                 </InputGroup>
               </Form.Group>
 
               <Form.Group controlId="password">
                 <InputGroup hasValidation>
-                  <Form.Label
-                    visuallyHidden
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label={t('login.forms.password')}
+                    className="mb-3"
                   >
-                    {t('login.forms.password')}
-                  </Form.Label>
-                  <Form.Control
-                    name="password"
-                    type="password"
-                    required
-                    placeholder={t('login.forms.password')}
-                    isInvalid={!!authError}
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                  />
-                  <Form.Control.Feedback type="invalid">
-                    {authError}
-                  </Form.Control.Feedback>
+                    <Form.Control
+                      name="password"
+                      type="password"
+                      required
+                      placeholder={t('login.forms.password')}
+                      isInvalid={!!authError}
+                      onChange={formik.handleChange}
+                      value={formik.values.password}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {authError}
+                    </Form.Control.Feedback>
+                  </FloatingLabel>
                 </InputGroup>
               </Form.Group>
 
               <Button
                 type="submit"
                 variant="primary"
-                className="primary_button"
+                className="auth_button"
               >
                 {t('login.pages_data.button')}
               </Button>
             </Form>
-          </Row>
-          <Row className="auth_footer">
-            {t('login.pages_data.footer_description')}
-            <a href="/signup" className="link">{t('login.pages_data.footer_link')}</a>
-          </Row>
-        </div>
-      </LayoutContainer>
+          </Col>
+        </Row>
+        <Row className="auth_footer">
+          {t('login.pages_data.footer_description')}
+          <a href="/signup" className="link">{t('login.pages_data.footer_link')}</a>
+        </Row>
+      </Container>
     </>
   );
 };
